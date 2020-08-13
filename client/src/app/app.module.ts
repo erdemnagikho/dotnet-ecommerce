8a997610a41b9fb57f8afe67b9fb264b09d90094
erdemnagikho <erdemnayir@gmail.com>
2020-08-13 14:23:38 +0300

    Building UI for Shop

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent],
